bdb7f5f04be55fd1f5b618d06be44f40cd4af6f6
chamgagnepoetry <champagnepoetrry@gmail.com>
2026-09-19 22:18:54 +1000

    Update LOGIC.lua

--- LOGIC.lua
+++ LOGIC.lua
@@ -1,242 +1,302 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/chamgagnepoetry/VantaLib/refs/heads/main/Library.lua"))()
-local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/chamgagnepoetry/VantaLib/refs/heads/main/addons/ThemeManager.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/chamgagnepoetry/VantaLib/refs/heads/main/addons/SaveManager.lua"))()
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
 
-local UI = {}
+local LocalPlayer = Players.LocalPlayer
 
-function UI:Initialize()
-    local Window = Library:CreateWindow({
-        Title = "SOLITAIRE UNIVERSAL";
-        Centre = true;
-        AutoShow = true;
-        TabPadding = 12;
-        MenuFadeTime = 0;
-    })
+local Mouse = LocalPlayer:GetMouse()
+local Camera = workspace.CurrentCamera
+Mouse.TargetFilter = LocalCharacter
 
-    local Tabs = {
-        Main = Window:AddTab("main");
-        Character = Window:AddTab("character");
-		Visuals = Window:AddTab("visuals");
-        Settings = Window:AddTab("settings");
-    }
+local LocalCharacter, LocalHumanoid, LocalHumanoidRootPart
+local NotificationTime = 3
 
-    -- \\MAIN TAB//
-    local CameraGroupBox = Tabs.Main:AddLeftGroupbox("camera")
-    local GunGroupBox = Tabs.Main:AddRightGroupbox("gun")
+local Logic = {}
 
-	-- Camera Group Box
-    CameraGroupBox:AddToggle("CameraLock", {
-        Text = "cam lock"
-    }):AddKeyPicker("CameraLockKey",{
-        Text = "cam lock";
-        Default = "E";
-        Mode = "Toggle";
-        NoUI = false;
-    })
-	CameraGroupBox:AddToggle("CameraFriendCheck",{
-		Text = "friend check"
-	})
-	CameraGroupBox:AddToggle("CameraWallCheck",{
-		Text = "wall check"
-	})
-	CameraGroupBox:AddToggle("CameraRadius",{
-		Text = "add radius"
-	})
-	local CameraRadiusDepBox = CameraGroupBox:AddDependencyBox()
-	CameraRadiusDepBox:SetupDependencies({
-		{Toggles.CameraRadius, true}
-	})
-	CameraRadiusDepBox:AddSlider("CameraRadiusSize",{
-		Text = "size";
-		Default = 200;
-		Min = 50;
-		Max = 800;
-		Rounding = 2;
-		Compact = false;
-	})
-	CameraGroupBox:AddDivider()
-	CameraGroupBox:AddDropdown("CameraLockToggleType",{
-		Values = {"Toggle","Hold"};
-		Default = 1;
-		Multi = false;
-		Text = "toggle type";
-		Callback = function(Value)
-			Options.CameraLockKey.Mode = tostring(Value)
-		end,
-	})
+local Library, Toggles, Options
 
-	-- Gun Group Box
+local function Setup(newCharacter)
+	LocalCharacter = newCharacter
+	LocalHumanoid = newCharacter:WaitForChild("Humanoid")
+	LocalHumanoidRootPart = newCharacter:WaitForChild("HumanoidRootPart")
 
-	GunGroupBox:AddToggle("GunTriggerBot",{
-		Text = "trigger bot"
-	}):AddKeyPicker("GunTriggerBotKey",{
-        Text = "trigger bot";
-        Default = "T";
-        Mode = "Toggle";
-        NoUI = false;
-    })
-	local TriggerBotDepBox = GunGroupBox:AddDependencyBox()
-	TriggerBotDepBox:SetupDependencies({
-		{Toggles.GunTriggerBot, true}
-	})
-	TriggerBotDepBox:AddToggle("GunTriggerBotFriendCheck",{
-		Text = "friend check"
-	})
-	GunGroupBox:AddDivider()
-
-    -- \\CHARACTER TAB//
-    local MovementGroupBox = Tabs.Character:AddLeftGroupbox("movement")
-
-	-- Movement Group Box
-    MovementGroupBox:AddToggle("VelocityToggle",{
-		Text = "velocity"
-	}):AddKeyPicker("VelocityKey",{
-		Text = "velocity";
-		Default = "X";
-		Mode = "Toggle";
-		NoUI = false;
-	})
-	MovementGroupBox:AddToggle("WalkSpeedToggle",{
-		Text = "walk speed",
-		Tooltip = "only works if the game has any sort of changing speed"
-	})
-	MovementGroupBox:AddToggle("JumpPowerToggle",{
-		Text = "jump power",
-		Tooltip = "only works if the game has any sort of changing jump power"
-	})
-	MovementGroupBox:AddSlider("VelocitySpeed",{
-		Text = "velocity speed";
-		Default = 99;
-		Min = 1;
-		Max = 1000;
-		Rounding = 0;
-		Compact = true;
-		Suffix = "s";
-	})
-	MovementGroupBox:AddSlider("WalkSpeed",{
-		Text = "walk speed";
-		Default = 16;
-		Min = 1;
-		Max = 1000;
-		Rounding = 0;
-		Compact = true;
-		Suffix = "s";
-	})
-	MovementGroupBox:AddSlider("JumpPower",{
-		Text = "jump power";
-		Default = 50;
-		Min = 1;
-		Max = 1000;
-		Rounding = 0;
-		Compact = true;
-		Suffix = "s";
-	})
-
-	-- \\VISUALS TAB//
-	local ESPGroupBox = Tabs.Visuals:AddLeftGroupbox("esp")
-	
-	-- Esp Group Box
-	ESPGroupBox:AddToggle("ESPBox",{
-	Text = "box";
-	}):AddColorPicker("ESPBoxColor", {
-		Default = Color3.fromRGB(255,255,255)
-	})
-	local ESPBoxDepBox = ESPGroupBox:AddDependencyBox()
-	ESPBoxDepBox:SetupDependencies({Toggles.ESPBox, true})
-	ESPBoxDepBox:AddToggle("ESPBoxOutline",{
-		Text = "outline"
-	})
-
-	ESPGroupBox:AddDivider()
-
-	ESPGroupBox:AddToggle("ESPName",{
-		Text = "name";
-	}):AddColorPicker("ESPNameColor", {
-		Default = Color3.fromRGB(255,255,255)
-	})
-	local ESPNameDepBox = ESPGroupBox:AddDependencyBox()
-	ESPNameDepBox:SetupDependencies({Toggles.ESPName, true})
-	ESPNameDepBox:AddToggle("ESPNameOutline",{
-		Text = "outline"
-	})
-
-	ESPGroupBox:AddDivider()
-
-	ESPGroupBox:AddToggle("ESPWeapon",{
-		Text = "weapon"
-	}):AddColorPicker("ESPWeaponColor",{
-		Default = Color3.fromRGB(255,255,255)
-	})
-	local ESPWeaponDepBox = ESPGroupBox:AddDependencyBox()
-	ESPWeaponDepBox:SetupDependencies({Toggles.ESPWeaponColor, true})
-	ESPWeaponDepBox:AddToggle("ESPWeaponOutline",{
-		Text = "outline"
-	})
-
-	ESPGroupBox:AddDivider()
-
-	ESPGroupBox:AddToggle("ESPDistance",{
-		Text = "distance"
-	}):AddColorPicker("ESPDistanceColor", {
-		Default = Color3.fromRGB(255,255,255)
-	})
-	local ESPDistanceDepBox = ESPGroupBox:AddDependencyBox()
-	ESPDistanceDepBox:SetupDependencies({Toggles.ESPDistance, true})
-	ESPDistanceDepBox:AddToggle("ESPDistanceOutline",{
-		Text = "outline"
-	})
-
-	ESPGroupBox:AddDivider()
-
-	local ESPHealthTG = ESPGroupBox:AddToggle("ESPHealth",{
-		Text = "healthbar"
-	})
-	ESPHealthTG:AddColorPicker("ESPHealthUpperColor",{
-		Default = Color3.fromRGB(60, 255, 100),
-		Title = "upper"
-	})
-	ESPHealthTG:AddColorPicker("ESPHealthMidColor",{
-		Default = Color3.fromRGB(255, 220, 60),
-		Title = "middle"
-	})
-	ESPHealthTG:AddColorPicker("ESPHealthLowerColor",{
-		Default = Color3.fromRGB(255, 60, 60),
-		Title = "lower"
-	})
-
-	ESPGroupBox:AddDivider()
-
-	ESPGroupBox:AddDropdown("ESPNametype",{
-		Text = "name type";
-		Values = {"username","displayname"};
-		Default = 1;
-		Mutli = false;
-	})
-
-    Library.KeybindFrame.Visible = true
-	Library.ToggleKeybind = Enum.KeyCode.RightControl
-
-	-- \\SETTINGS TAB//
-
-	local RainbowGroupBox = Tabs.Settings:AddLeftGroupbox("rainbow setting")
-	RainbowGroupBox:AddRainbowAccentToggle("RainbowAccent",{
-		Text = "rainbow accent";
-		Default = false;
-	})
-
-	SaveManager:SetLibrary(Library)
-	ThemeManager:SetLibrary(Library)
-	SaveManager:IgnoreThemeSettings()
-	SaveManager:SetIgnoreIndexes({ "MenuKeybind" })
-	SaveManager:BuildConfigSection(Tabs.Settings)
-	SaveManager:LoadAutoloadConfig()
-	ThemeManager:ApplyTheme("Solitaire Universal")
-
-	return {
-		Library = Library,
-		Toggles = Toggles,
-		Options = Options,
-	}
+	Mouse.TargetFilter = newCharacter
 end
 
-return UI
+LocalPlayer.CharacterAdded:Connect(Setup)
+Setup(LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait())
+
+local function getEquippedTool()
+	local Tool
+	for _, object in pairs(LocalCharacter:GetChildren()) do
+		if object:IsA("Tool") then
+			Tool = object
+			return Tool
+		end
+	end
+	return false
+end
+
+local function checkWall(target)
+	if not LocalHumanoidRootPart then
+		return false
+	end
+
+	local TargetCharacter = target.Character
+	if not TargetCharacter then
+		return false
+	end
+
+	local TargetRootPart = TargetCharacter:FindFirstChild("HumanoidRootPart")
+	if not TargetRootPart then
+		return false
+	end
+
+	local Origin = LocalHumanoidRootPart.Position
+	local Direction = TargetRootPart.Position - Origin
+
+	local Params = RaycastParams.new()
+	Params.FilterType = Enum.RaycastFilterType.Exclude
+	Params.FilterDescendantsInstances = {
+		LocalCharacter,
+		TargetCharacter
+	}
+
+	local Result = workspace:Raycast(Origin, Direction, Params)
+
+	return Result ~= nil
+end
+
+local function checkFriend(target)
+	return LocalPlayer:IsFriendsWith(target.UserId)
+end
+
+local function FindPlayerToMouse(radius,configurations)
+	if not LocalHumanoidRootPart then
+        return false
+    end
+	local ClosestPlayer = nil
+	local shortestDistance = math.huge
+	local mousePos = Vector2.new(Mouse.X, Mouse.Y)
+	for _, target in ipairs(Players:GetPlayers()) do
+		if target ~= LocalPlayer and target.Character then
+			local targetRootPart = target.Character:FindFirstChild("HumanoidRootPart")
+			if targetRootPart then
+				if configurations.CameraFriendCheck and checkFriend(target) then
+					continue
+				end
+
+				if configurations.CameraWallCheck and checkWall(target) then
+					continue
+				end
+
+				local screenPos, onScreen = Camera:WorldToViewportPoint(targetRootPart.Position)
+				if onScreen then
+					local screenVector = Vector2.new(screenPos.X, screenPos.Y)
+					local distance = (screenVector - mousePos).Magnitude
+					if distance < shortestDistance then
+						shortestDistance = distance
+						ClosestPlayer = target
+					end
+				end
+			end
+		end
+	end
+	return ClosestPlayer
+end
+
+local function runLoop(controlObj, loopFn, stopFn)
+	if type(controlObj) == "string" then
+		local keybind
+
+		if controlObj:sub(1, 1) == "!" then
+			keybind = Options[controlObj:sub(2)]
+
+			task.spawn(function()
+				pcall(function()
+					while true do
+						local state = keybind:GetState()
+
+						if state then
+							loopFn()
+						else
+							if stopFn then
+								stopFn()
+							end
+						end
+
+						RunService.Heartbeat:Wait()
+					end
+				end)
+			end)
+		else
+			keybind = Options[controlObj]
+
+			task.spawn(function()
+				pcall(function()
+					while true do
+						local state = keybind:GetState()
+
+						if state then
+							loopFn()
+						else
+							if stopFn then
+								stopFn()
+							end
+						end
+
+						task.wait()
+					end
+				end)
+			end)
+		end
+
+		return
+	end
+
+	if controlObj.OnChanged then
+		local loopTask
+
+		controlObj:OnChanged(function()
+			if controlObj.Value then
+				if not loopTask then
+					loopTask = task.spawn(function()
+						pcall(function()
+							while controlObj.Value do
+								task.wait()
+								loopFn()
+							end
+
+							loopTask = nil
+
+							if stopFn then
+								stopFn()
+							end
+						end)
+					end)
+				end
+			else
+				loopTask = nil
+
+				if stopFn then
+					stopFn()
+				end
+			end
+		end)
+	end
+end
+
+function Logic:Initialize(UIReference)
+    _G.Toggles = UIReference.Toggles
+	_G.Options = UIReference.Options
+	Toggles = UIReference.Toggles
+	Options = UIReference.Options
+	Library = UIReference.Library
+
+	--[[pcall(function()
+		LPH_NO_VIRTUALIZE = function(...) return (...) end
+		local newindex; newindex = hookmetamethod(game, "__newindex", LPH_NO_VIRTUALIZE(function(self, key, value)
+			if key == 'WalkSpeed' then 
+				if Toggles.WalkSpeedToggle.Value then
+					value = Options.WalkSpeed.Value
+				end
+			end
+			if key == "JumpPower" then 
+				if Toggles.JumpPowerToggle.Value then
+					value = Options.JumpPower.Value
+				end
+			end
+			return newindex(self, key, value)
+		end))
+	end)--]]
+	-- MAIN TAB
+	local CameraTarget = nil
+	runLoop("CameraLockKey", function()
+		if not CameraTarget then
+			CameraTarget = FindPlayerToMouse(nil, {
+				CameraFriendCheck = Toggles.CameraFriendCheck.Value,
+				CameraWallCheck = Toggles.CameraWallCheck.Value
+			})
+		end
+	
+		if CameraTarget and CameraTarget.Character then
+			local TargetHeadPart = CameraTarget.Character:FindFirstChild("Head")
+	
+			if TargetHeadPart then
+				Camera.CFrame = CFrame.new(
+					Camera.CFrame.Position,
+					TargetHeadPart.Position
+				)
+			end
+		end
+	end, function()
+		CameraTarget = nil
+	end)
+
+	runLoop("GunTriggerBotKey", function()
+		local Tool = getEquippedTool()
+		if not Tool then
+			return
+		end
+	
+		local TargetPart = Mouse.Target
+		if not TargetPart then
+			return
+		end
+	
+		local TargetModel = TargetPart.Parent
+		if not TargetModel then
+			return
+		end
+	
+		local TargetHumanoid = TargetModel:FindFirstChildOfClass("Humanoid")
+	
+		if not TargetHumanoid and TargetModel.Parent then
+			TargetHumanoid = TargetModel.Parent:FindFirstChildOfClass("Humanoid")
+			if TargetHumanoid then
+				TargetModel = TargetModel.Parent
+			end
+		end
+	
+		if not TargetHumanoid then
+			return
+		end
+	
+		local TargetPlayer = Players:FindFirstChild(TargetModel.Name)
+		local FriendCheck = false
+	
+		if Toggles.GunTriggerBotFriendCheck.Value then
+			FriendCheck = checkFriend(TargetPlayer)
+		end
+
+		if not FriendCheck then
+			mouse1click()
+		end
+	end)
+
+	-- CHARACTER TAB
+    runLoop("VelocityKey", function()
+		if LocalHumanoid and LocalHumanoidRootPart then
+			local direction = LocalHumanoid.MoveDirection
+			local velocity = LocalHumanoidRootPart.AssemblyLinearVelocity
+			local vertical = velocity.Y
+			local horizontal = direction * Options.VelocitySpeed.Value
+			LocalHumanoidRootPart.AssemblyLinearVelocity = Vector3.new(horizontal.X,vertical, horizontal.Z)
+		end
+	end)
+
+	Toggles.WalkSpeedToggle:OnChanged(function()
+		if Toggles.WalkSpeedToggle.Value then
+			--Library:Notify("USE AT YOUR OWN RISK!", NotificationTime)
+			Library:Notify("DISABLED UNTIL FURTHER NOTICE (Security Reasons)", NotificationTime)
+		end
+	end)
+
+	Toggles.JumpPowerToggle:OnChanged(function()
+		if Toggles.JumpPowerToggle.Value then
+			--Library:Notify("USE AT YOUR OWN RISK!", NotificationTime)
+			Library:Notify("DISABLED UNTIL FURTHER NOTICE (Security Reasons)", NotificationTime)
+		end
+	end)
+end
+
+return Logic
