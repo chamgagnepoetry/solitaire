732cea4349f56ed800a8af0f8d01951fe06a5ef7
chamgagnepoetry <champagnepoetrry@gmail.com>
2026-09-22 02:25:15 +1000

    Update UI.lua

--- universal/UI.lua
+++ universal/UI.lua
@@ -9,7 +9,7 @@ local UI = {}
 
 function UI:Initialize()
     local Window = Library:CreateWindow({
-        Title = "SOLITAIRE ON GEAR";
+        Title = "SOLITAIRE BOOSTED";
 		SubTitle = "universal";
         Centre = true;
         AutoShow = true;
